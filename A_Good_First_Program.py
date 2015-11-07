edc61fc541b02e2920c5bbaa541280253735cb5b
anash28 <alfredzo.nash@gmail.com>
2015-11-06 23:03:05 -0500

    Archive of examples started 10/25/14

---
+++ A_Good_First_Program.py
@@ -0,0 +1,17 @@
+#LPTHW By Zed Shaw
+
+
+
+
+#LPTHW Exersize 3 Numbers and Math
+
+## LPTHW Exersize 1 
+
+#print "Hello World"
+#print "Hello again"
+#print "I like typing this."
+#print "This is fun"
+#print "Yay! Printing."
+#print "I`d much rather you 'not'."
+#print 'I "said" do not touch this.'
+print 'Zed Shaw you are a little arrogant brah'
---
+++ Numbers_and_Math.py
@@ -0,0 +1,5 @@
+#LPTHW By Zed Shaw
+
+#LPTHW Exersize 3 Numbers and Math
+
+
