632324a38ea929dd10a45f91f420822965a947c5
Al Nash <anash@datto.com>
2015-11-09 13:31:43 -0500

    Chomping through Ex7.

---
+++ exercise7/ex7_studydrill.md
@@ -0,0 +1,11 @@
+#Exercise 7 Study Drill 
+
+1. Go through and write a comment on what each line does 
+
+2. Read each one backward or out loud to find your errors. 
+
+3. From now on , when you make mistakes, write down on a piece of paper what kind of mistakes you made. 
+
+4 When you go to the next exercise, look at the last mistakes you made and try not to make them in this new one. 
+
+5 Remember that everyone makes mistakes. Programmers are like magicians who like everyone to think they are perfect and never wrong, but its all an act. They make mistakes all the time. 
---
+++ exercise7/more_printing.py
@@ -0,0 +1,27 @@
+# Exercise 7 More Printing 
+# A bunch of exercises where youy just type code in and make 
+# it run. I won`t be explaining much since it is just more of the same 
+# The purpose is to build up your chops. Do Not Skip! Do Not Paste! 
+
+
+print "Mary had a little lamb."
+print "Its fleece ws white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # what`d that do? 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g" 
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, 
+print end7 + end8 + end9 + end10 + end11 + end12 
