f12bd9b260de3e5b5a97faaa480fe4eab0db5035
anash28 <alfredzo.nash@gmail.com>
2015-11-09 18:38:10 -0500

    Added files created during lunch @datto

---
+++ exercise7 /more_printing.py
@@ -0,0 +1,4 @@
+# Exercise 7: More Printing 
+
+# I wont be explaining much since it is just more of the same. The purpose is to build up
+# your chops. See you in a few exercises, and do not skip! Do not paste! 
